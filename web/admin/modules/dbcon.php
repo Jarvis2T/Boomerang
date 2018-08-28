4a4cb254455c04aee2d6f04eec0e252256651a07
Thanh Truong <jarvis.tr1109@gmail.com>
2018-08-28 15:59:26 +1200

    dbcon add test

--- web/admin/modules/dbcon.php
+++ web/admin/modules/dbcon.php
@@ -1,8 +1,8 @@
 <?php  
-	$user = 'root';
-	$pass = '';
-	$db = 'boomerang';
+	$user = 'b36e7bafe7df03';
+	$pass = '069b3a9c';
+	$db = 'heroku_a5a5c56bf5174f4';
 
-	$conn = mysqli_connect('localhost', $user, $pass, $db) ;
-	$conn->set_charset("utf8");
+	$conn = mysqli_connect('us-cdbr-iron-east-01.cleardb.net', $user, $pass, $db) ;
+	//$conn->set_charset("utf8");
 ?>
